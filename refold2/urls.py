64513c68cdd783aa5fd195db67bcd921f6c1bef7
Steve Androulakis <steve.androulakis@gmail.com>
2012-01-25 11:49:41 +1100

    Added DigitalContentData XML Feed For Thompson Reuters, author field doesn't work yet

--- refold2/urls.py
+++ refold2/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from refold.models import RefoldingRecord
 from refold.RefoldingRecordListView import RefoldingRecordListView
 from refold.RefoldingRecordByProteinListView import RefoldingRecordByProteinListView
@@ -25,6 +25,12 @@ urlpatterns = patterns('',
         DetailView.as_view(
             model=RefoldingRecord,
     )),
+    
+    (r'^refoldingrecord/digitalcontentdata/$',
+        ListView.as_view(
+            model=RefoldingRecord,
+            template_name="refold/refoldingrecord_digitalcontentdata.html",
+    )),    
 
     (r'^protein/(\w+)/$',
         RefoldingRecordByProteinListView.as_view(
