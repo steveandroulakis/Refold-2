d0351284bd4eeca6c1035a9a823f3714c9687a42
Steve Androulakis <steve.androulakis@gmail.com>
2012-06-26 10:59:29 +1000

    Inexact search. For realsies this time (still needs the old one deprec)

--- refold2/refold/RefoldingRecordListView.py
+++ refold2/refold/RefoldingRecordListView.py
@@ -1,8 +1,28 @@
 from django.views.generic import ListView
 from refold2.refold.models import RefoldingRecord
+import urlparse
 
 class RefoldingRecordListView(ListView):
 
-    model = RefoldingRecord      #implies -> queryset = models.Car.objects.all()
+    # model = RefoldingRecord      #implies -> queryset = models.Car.objects.all()
     context_object_name = "refoldingrecord_list"    #default is object_list
     paginate_by = 25  #and that's it !!
+    protein = None
+
+    def get_queryset(self):
+        rr = RefoldingRecord.objects.all()
+        
+        self.protein = self.request.GET.get('protein')
+
+        if self.protein:
+            protein_unencoded = urlparse.unquote(self.protein)
+            rr = rr.filter(construct__homologue__protein__name__icontains=protein_unencoded)
+        
+        return rr
+        
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(RefoldingRecordListView, self).get_context_data(**kwargs)
+        # Add in the publisher
+        context['searchterm'] = self.protein
+        return context
